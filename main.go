a02f46da4b4fca51a66ce595356f5829bbc0e5e2
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2025-03-10 20:30:10 +0900

    基本的なパケットの構造と送受信処理を実装。

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+// 基本的なパケット構造体
+type Packet struct {
+	Data string
+}
+
+// {Data: data}のdataのみを返すために書いている
+func (p Packet) String() string {
+	return p.Data
+}
+
+// Packetを引数で受け取り送信する
+func sendPacket(p Packet) {
+	fmt.Printf("Sending packet: %s\n", p)
+}
+
+func receivePacket(p Packet) {
+	fmt.Printf("Received packet: %s\n", p)
+}
+
+// Packetを作成、送信と受信を行う。
+func main() {
+	packet := Packet{Data: "Hello ,Network!!"}
+	sendPacket(packet)
+	receivePacket(packet)
+}
