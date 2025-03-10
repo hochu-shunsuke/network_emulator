83ffc83e0baeded4250c3e98e726e488c18a0369
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2025-03-10 23:40:53 +0900

    host間でswitchを介した通信のモックを実装。まだ独立した関数の呼び出しでしかないので要改善。

--- main.go
+++ main.go
@@ -102,6 +102,51 @@ func (h *Host) GetName() string {
 	return h.Name
 }
 
+// Switch（Deviceインターフェースを満たす）
+type Switch struct {
+	Name  string
+	Ports map[string]Device
+}
+
+func (s *Switch) SendPacket(p Packet) {
+	if dst, exists := s.Ports[p.DstMAC]; exists { //dst, existsで、値と存在の2つを返す
+		fmt.Printf("[Switch] Forwarding packet to %s\n", p.DstMAC)
+		dst.ReceivePacket(p)
+	} else {
+		fmt.Printf("[Switch] Destination MAC %s not found\n", p.DstMAC)
+	}
+}
+
+func (s *Switch) ReceivePacket(p Packet) {
+	s.SendPacket(p)
+}
+
+func (s *Switch) GetName() string {
+	return s.Name
+}
+
+// Router（Deviceインターフェースを満たす）
+type Router struct {
+	Name  string
+	Ports map[string]Device
+}
+
+func (r *Router) SendPacket(p Packet) {
+	if nextHop, exists := r.Ports[p.DstIP]; exists {
+		fmt.Printf("[Router] Forwarding packet to %s\n", p.DstIP)
+		nextHop.ReceivePacket(p)
+	} else {
+		fmt.Printf("[Router] No route to %s\n", p.DstIP)
+	}
+}
+func (r *Router) ReceivePacket(p Packet) {
+	r.SendPacket(p)
+}
+
+func (r *Router) GetName() string {
+	return r.Name
+}
+
 // Packetを作成、送信と受信を行う。
 func main() {
 	host1 := &Host{Name: "Host1", Layers: []Layer{
@@ -112,7 +157,21 @@ func main() {
 		&DataLinkLayer{Name: "DataLink", MAC: "AA:BB:CC:DD:EE:02"},
 		&NetworkLayer{Name: "Network", IP: "192.168.1.2"},
 	}}
+	switch1 := &Switch{Name: "Switch1", Ports: map[string]Device{"AA:BB:CC:DD:EE:01": host1, "AA:BB:CC:DD:EE:02": host2}}
 	packet := Packet{Data: "Hello Network!!", SrcIP: "192.168.1.1", DstIP: "192.168.1.2", SrcMAC: "AA:BB:CC:DD:EE:01", DstMAC: "AA:BB:CC:DD:EE:02"}
 	host1.SendPacket(packet)
-	host2.ReceivePacket(packet)
+	switch1.ReceivePacket(packet)
 }
+
+//現在の問題点
+/*
+162、163行目に記述していることを確認すると、
+host1がsendするとswitch1がreceiveする。
+switch1はパケットの宛先MACアドレスを確認して、そのホストのreceive関数を呼び出している。
+そのため、実際には通信のモックが行われている。
+
+通信ではなく、独立した関数の呼び出しになっているため、
+改善しないと支障が出ると予想するし、何よりロマンがない
+
+改善方法についてはまだ未定。
+*/
