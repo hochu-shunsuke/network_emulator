11dab980a44a0c4f7d556bfe7f9493d5e3f3493d
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2025-03-10 21:02:17 +0900

    ホストを導入し、パケットを送受信する主体を定義。

--- main.go
+++ main.go
@@ -12,18 +12,35 @@ func (p Packet) String() string {
 	return p.Data
 }
 
-// Packetを引数で受け取り送信する
-func sendPacket(p Packet) {
-	fmt.Printf("Sending packet: %s\n", p)
+// ネットワークデバイスのインターフェース（パケットの送受信を定義）
+type Device interface {
+	SendPacket(p Packet)    //パケット送信メソッド
+	ReceivePacket(p Packet) //パケット受信メソッド
+	GetName() string        //デバイス名取得メソッド
 }
 
-func receivePacket(p Packet) {
-	fmt.Printf("Received packet: %s\n", p)
+type Host struct {
+	Name string
+}
+
+// Packetを引数で受け取り送信する
+func (h *Host) SendPacket(p Packet) {
+	fmt.Printf("%s is sending packet: %s\n", h.Name, p)
+}
+
+func (h *Host) ReceivePacket(p Packet) {
+	fmt.Printf("%s is receiving packet: %s\n", h.Name, p)
+}
+
+func (h *Host) GetName() string {
+	return h.Name
 }
 
 // Packetを作成、送信と受信を行う。
 func main() {
+	host1 := &Host{Name: "Host1"}
+	host2 := &Host{Name: "Host2"}
 	packet := Packet{Data: "Hello ,Network!!"}
-	sendPacket(packet)
-	receivePacket(packet)
+	host1.SendPacket(packet)
+	host2.ReceivePacket(packet)
 }
